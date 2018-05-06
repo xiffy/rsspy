4da9e5267c65015c0b85a340068d2ee28483ff15
xiffy <a.verschoor@nrc.nl>
2018-05-07 01:19:38 +0200

    toggle, sort of.

--- rsspy/static/js/rsspy.js
+++ rsspy/static/js/rsspy.js
@@ -1,5 +1,14 @@
 $( document ).ready(function() {
 	$('.menudrop').on('click', function() {
 		$('.slider').toggle('slide', 150);
-	})
+	});
+
+	$('a.hide-feeds').click( function() {
+		that = this
+    groupid = $(this).data('groupid');
+    $('.group-feeds-feed'+groupid).each( function() {
+    	$(this).toggle();
+    });
+	});
 });
+
