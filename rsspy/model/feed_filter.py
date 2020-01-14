13de4ebb7c13a4b39d6d9bf68395ff86807c6e9d
xiffy <xiffy@xiffy.nl>
2020-01-14 01:06:37 +0100

    Starting some content transformations part iii.

---
+++ rsspy/model/feed_filter.py
@@ -0,0 +1,23 @@
+from . import db as dbase
+import ast
+
+
+class FeedFilter:
+    def __init__(self, ID=None, feedID=None, content_filter=None):
+        self.db = dbase.DBase()
+        self.ID = ID
+        self.feedID = feedID
+        self.content_filter=content_filter
+        if feedID or ID:
+            self._get(ID=ID, feedID=feedID)
+
+    def _get(self, ID=None, feedID=None):
+        if ID:
+            self.db.cur.execute('select * from `feed_filter` where ID = %d' % ID)
+        elif feedID:
+            self.db.cur.execute('select * from `feed_filter` where feedID = %d' % feedID)
+        row = self.db.cur.fetchone()
+        if row:
+            self.ID, self.feedID, self.content_filter = row
+
+
