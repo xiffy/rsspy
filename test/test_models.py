edacb1fbb1fa5118eadd39ee4a6a94789b3c2ec1
xiffy <github@xiffy.nl>
2023-01-03 18:36:47 +0100

    andere imports voorzichtig doorvoeren

--- test/test_models.py
+++ test/test_models.py
@@ -1,42 +1,42 @@
 import unittest
-from rsspy.model import feed
-from rsspy.model import bookmark
-from rsspy.model import entry
-from rsspy.model import user
-from rsspy.model import group
-from rsspy.model import group_feed
+from rsspy.model.feed import Feed
+from rsspy.model.bookmark import Bookmark
+from rsspy.model.entry import Entry
+from rsspy.model.user import User
+from rsspy.model.group import Group
+from rsspy.model.group_feed import GroupFeed
 
 
 class TestFeed(unittest.TestCase):
     def test_init(self):
-        f = feed.Feed()
+        f = Feed()
         self.assertEqual(f.ID, None)
 
     def test_init_with_id(self):
-        f = feed.Feed(2)
+        f = Feed(2)
         self.assertEqual(f.url, "https://www.nrc.nl/rss/")
 
     def test_with_entries(self):
-        f = feed.Feed(2)
+        f = Feed(2)
         f.with_entries()
         self.assertGreater(len(f.entries), 2)
 
     def test_get_all(self):
-        f = feed.Feed()
+        f = Feed()
         self.assertGreater(len(f.get_all()), 2)
 
 
 class TestBookmark(unittest.TestCase):
     def test_init(self):
-        b = bookmark.Bookmark()
+        b = Bookmark()
         self.assertEqual(b.ID, None)
 
     def test_gat_bookamrks(self):
-        b = bookmark.Bookmark()
+        b = Bookmark()
         self.assertGreater(len(b.get_bookmarks(1)), 2)
 
     def test_bookmarked(self):
-        b = bookmark.Bookmark()
+        b = Bookmark()
         self.assertIsNone(b.bookmarked(userID=1, entryID=1))
         self.assertFalse(b.bookmarked(userID=None, entryID=1))
         self.assertFalse(b.bookmarked(userID=1, entryID=None))
@@ -45,54 +45,54 @@ class TestBookmark(unittest.TestCase):
 
 class TestEntry(unittest.TestCase):
     def test_init(self):
-        e = entry.Entry()
+        e = Entry()
         self.assertEqual(e.ID, None)
 
     def test_init_with_id(self):
-        e = entry.Entry(28160)
+        e = Entry(28160)
         self.assertEqual(e.title, "Death to America")
 
     def test_fetch_by_feed(self):
-        e = entry.Entry()
+        e = Entry()
         rows = e.fetch_by_feed(2)
         self.assertGreater(len(rows), 3)
 
 
 class TestGroup(unittest.TestCase):
     def test_init(self):
-        g = group.Group()
+        g = Group()
         self.assertEqual(g.ID, None)
 
     def test_init_with_id(self):
-        g = group.Group(1)
+        g = Group(1)
         self.assertEqual(g.description, "Comics")
 
     def test_feeds(self):
-        g = group.Group(2)
+        g = Group(2)
         self.assertGreater(len(g.feeds), 1)
 
 
 class TestGroupFeed(unittest.TestCase):
     def test_init(self):
-        g = group_feed.GroupFeed()
+        g = GroupFeed()
         self.assertEqual(g.ID, None)
-        self.assertIsInstance(g, group_feed.GroupFeed)
+        self.assertIsInstance(g, GroupFeed)
 
     def test_get_feeds(self):
-        g = group_feed.GroupFeed(1)
+        g = GroupFeed(1)
         self.assertEqual(g.ID, 1)
         self.assertGreater(len(g.get_feeds(groupID=1)), 2)
 
 
 class TestUser(unittest.TestCase):
     def test_init(self):
-        u = user.User()
+        u = User()
         self.assertEqual(u.username, None)
-        self.assertIsInstance(u, user.User)
+        self.assertIsInstance(u, User)
         assert getattr(u, "ID", "Undefined") == "Undefined"
 
     def test_be_someone(self):
-        u = user.User(1)
+        u = User(1)
         self.assertEqual(u.username, "xiffy")
         self.assertGreater(len(u.bookmarks), 3)
 
