0ba3178887c398f3f15482ef152e8e6b9a284382
Appie Verschoor <xiffy@molecule.nl>
2025-01-10 19:35:06 +0100

    better off

--- rsspy/rsspy.py
+++ rsspy/rsspy.py
@@ -204,7 +204,7 @@ def groupfeed():
         result = GroupFeed(
             feedID=request.form.get("feedid"), groupID=request.form.get("groupid")
         )
-    return jsonify(result)
+    return jsonify(result.ID)
 
 
 def remove_groupfeed(groupID=None, feedID=None):
